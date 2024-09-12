561e7364cd3dbe87245dc3677ceb89ab1e580525
Cansu Aydın <cansu@Cansu-MacBook-Pro.local>
2024-09-12 19:46:29 +0300

    deleteAPI ve putAPI eklendi

--- services/fetchAPI/index.jsx
+++ services/fetchAPI/index.jsx
@@ -14,9 +14,6 @@ const postAPI = async (
       headers: headers,
       body: JSON.stringify(body),
       cache: "no-store",
-      // cache önemli! her çalıştığında cache'deki veri yerine -> güncel veriyi almasını sağlar.
-      // bu olmaz ise üncel veriyi almayabiliyor dikkat et.
-      // Dinamik sayfalarda burası kullanılıyorsa o sayfalara -> export const dynamic = 'force-dynamic' ekle!
     })
       .then((res) => {
         if (res.url.includes("/notification") && res.redirected) {
@@ -33,7 +30,7 @@ const postAPI = async (
   }
 };
 
-// Öğrenci (kayıt) işlemleri için kullanılan servis
+// GET
 const getAPI = async (
   URL,
   headers = { "Content-Type": "application/json" }
@@ -56,4 +53,67 @@ const getAPI = async (
   return data;
 };
 
-export { postAPI, getAPI };
+// PUT
+const putAPI = async (
+  URL,
+  body,
+  method = "PUT",
+  headers = { "Content-Type": "application/json" }
+) => {
+  try {
+    if (!process.env.NEXT_PUBLIC_API_URL || !URL) {
+      throw new Error("URL bulunamadı!");
+    }
+    const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL + URL}`, {
+      method: method,
+      headers: headers,
+      body: JSON.stringify(body),
+      cache: "no-store",
+    })
+      .then((res) => {
+        if (res.url.includes("/notification") && res.redirected) {
+          return (window.location.href = res.url);
+        } else {
+          return res.json();
+        }
+      })
+      .catch((err) => console.log(err));
+
+    return data;
+  } catch (err) {
+    throw new Error(`API request failed: ${err}`);
+  }
+};
+
+// DELETE
+const deleteAPI = async (
+  URL,
+  method = "DELETE",
+  headers = { "Content-Type": "application/json" }
+) => {
+  try {
+    if (!process.env.NEXT_PUBLIC_API_URL || !URL) {
+      throw new Error("URL bulunamadı!");
+    }
+    const data = await fetch(`${process.env.NEXT_PUBLIC_API_URL + URL}`, {
+      method: method,
+      headers: headers,
+      cache: "no-store",
+    })
+      .then((res) => {
+        if (res.url.includes("/notification") && res.redirected) {
+          return (window.location.href = res.url);
+        } else {
+          return res.json();
+        }
+      })
+      .catch((err) => console.log(err));
+
+    return data;
+  } catch (err) {
+    throw new Error(`API request failed: ${err}`);
+  }
+};
+console.log(URL);
+
+export { postAPI, getAPI, putAPI, deleteAPI };
